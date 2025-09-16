b8bdc362250a8901df57367ba69867985206b211
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-09-16 12:32:35 +0500

    Update image for App Development Club Lead in OurTeam component

--- src/components/About/OurTeam.jsx
+++ src/components/About/OurTeam.jsx
@@ -33,7 +33,7 @@ const team = {
         { name: "Muhammad Mamoon Khan", role: "Software Engineering Club Lead", image: "/images/developers/mamoon.jpg" },
         { name: "Jafar Ali", role: "Cyber Security Club Lead", image: "/images/team/cyber-lead.jpg" },
         { name: "Muhammad Ilyas", role: "AI & DS Club Lead", image: "/images/team/ai-lead.jpg" },
-        { name: "Hamza Ahmed Khan", role: "App Development Club Lead", image: "/images/team/app-lead.jpg" },
+        { name: "Hamza Ahmed Khan", role: "App Development Club Lead", image: "/images/team/app-lead2.jpg" },
         { name: "Shahab Saqib", role: "PR Head", image: "/images/team/pr-lead-2.jpg" },
         { name: "Iqra Noor", role: "Content & Graphics Head", image: "/images/team/graphic-head.jpg" },
     ],
