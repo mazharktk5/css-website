605309bc843ed2713d4b23f7020792852e4af8e9
nida <nidaandsalwakhan@gmail.com>
2026-02-02 22:59:18 +0500

    changed the front page

--- src/data/developers.js
+++ src/data/developers.js
@@ -1,28 +1,28 @@
 const developers = [
     {
-        name: "Muhammad Mamoon Khan",
-        role: "Lead / Supervisor",
-        image: "/images/developers/mamoon.jpg",
-        linkedin: "https://www.linkedin.com/in/muhammadmamoonkhan/",
-    },
-    {
         name: "Mazhar Ahmad",
-        role: "MERN Developer",
+        role: "Lead / Supervisor",
         image: "/images/developers/mazhar.jpg",
-        linkedin: "www.linkedin.com/in/mazhar-ahmad78",
+        linkedin: "https://www.linkedin.com/in/mazhar-ahmad78?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     },
     {
-        name: "Muhammad Hanzala Dawood",
+        name: "Nida Khan",
         role: "MERN Developer",
-        image: "/images/developers/hanzala.jpg",
-        linkedin: "https://www.linkedin.com/in/mhk47/",
+        image: "/images/developers/nida.png",
+        linkedin: "https://www.linkedin.com/in/nida-khan106?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     },
-    {
-        name: "Safia Zulfiqar",
-        role: "Frontend Developer",
-        image: "/images/developers/safia.jpg",
-        linkedin: "https://www.linkedin.com/in/safia-zulfiqar-292047327/",
-    },
+    // {
+    //     name: "Musa",
+    //     role: "MERN Developer",
+    //     image: "/images/developers/hanzala.jpg",
+    //     linkedin: "https://www.linkedin.com/in/mhk47/",
+    // },
+    // {
+    //     name: "",
+    //     role: "Frontend Developer",
+    //     image: "/images/developers/safia.jpg",
+    //     linkedin: "https://www.linkedin.com/in/safia-zulfiqar-292047327/",
+    // },
 ];
 
 export default developers;
